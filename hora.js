6bc686a4c467fafcb265a32bf7520f1bbb83ace3
Naoki050 <nayara.sanchez.ortiz.050@gmail.com>
2025-11-17 13:28:22 +0100

    Update hora.js

--- hora.js
+++ hora.js
@@ -4,14 +4,11 @@ function mostrarHora() {
   var minutos = ahora.getMinutes();
   var segundos = ahora.getSeconds();
 
-  // Si minutos o segundos son menores de 10, añadimos un 0 delante
   if (minutos < 10) minutos = "0" + minutos;
   if (segundos < 10) segundos = "0" + segundos;
 
-  // Mostramos la hora en el elemento con id="hora"
   document.getElementById("hora").innerHTML =
     "HORA: " + horas + ":" + minutos + ":" + segundos;
 }
 
-// Llamamos a la función cada segundo (1000 milisegundos)
 setInterval(mostrarHora, 1000);
